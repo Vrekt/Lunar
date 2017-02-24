a377ec75006bc053aedbf91eda682c4589f200f8
Vrekt <waffles4333@gmail.com>
2017-02-24 12:49:35 -0600

    Added an animation system

---
+++ src/me/vrekt/lunar/animation/Animation.java
@@ -0,0 +1,106 @@
+package me.vrekt.lunar.animation;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+public class Animation {
+
+	private int frameCount;
+	private int currentFrame;
+	private int frameDelay;
+
+	private boolean running = false;
+	private BufferedImage[] frames;
+
+	private boolean loop = false;
+
+	/**
+	 * Initialize the animation.
+	 * 
+	 * @param animations
+	 * @param perFrameDelay
+	 * @param loop
+	 */
+	public Animation(BufferedImage[] animations, int perFrameDelay, boolean loop) {
+		frames = new BufferedImage[animations.length];
+		for (int i = 0; i < frames.length; i++) {
+			frames[i] = animations[i];
+		}
+
+		currentFrame = 0;
+		frameCount = 0;
+
+		this.frameDelay = perFrameDelay;
+		this.loop = loop;
+
+	}
+
+	/**
+	 * Start the animation.
+	 */
+	public void startAnimation() {
+		running = true;
+	}
+
+	/**
+	 * Stop the animation.
+	 */
+	public void stopAnimation() {
+		running = false;
+	}
+
+	/**
+	 * Update the current frame.
+	 */
+	public void updateAnimation() {
+		if (running) {
+			frameCount++;
+			if (frameCount >= frameDelay) {
+				frameCount = 0;
+
+				currentFrame++;
+				if (currentFrame >= frames.length) {
+					if (loop) {
+						currentFrame = 0;
+						return;
+					}
+					stopAnimation();
+					currentFrame = 0;
+				}
+
+			}
+
+		}
+
+	}
+
+	/**
+	 * Get the current frame.
+	 * 
+	 * @return
+	 */
+	public int getCurrentFrame() {
+		return currentFrame;
+	}
+
+	/**
+	 * Draw the current frame.
+	 * 
+	 * @param graphics
+	 * @param x
+	 * @param y
+	 */
+	public void drawCurrentFrame(Graphics graphics, int x, int y) {
+		graphics.drawImage(frames[currentFrame], x, y, null);
+	}
+
+	/**
+	 * Set if we should loop or not.
+	 * 
+	 * @param loop
+	 */
+	public void setLoop(boolean loop) {
+		this.loop = loop;
+	}
+
+}
--- src/me/vrekt/lunar/sprite/SpriteManager.java
+++ src/me/vrekt/lunar/sprite/SpriteManager.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
+import me.vrekt.lunar.world.dir.Direction;
+
 public class SpriteManager {
 
 	private BufferedImage spriteSheet;
@@ -39,6 +41,55 @@ public class SpriteManager {
 	 */
 	public BufferedImage getSectionAt(int x, int y, int width, int height) {
 		return spriteSheet.getSubimage(x, y, width, height);
+	}
+
+	/**
+	 * Returns an array of Images for animations. Using this would be perfect
+	 * for getting multiple sequences of images for animations.
+	 * 
+	 * @param x
+	 * @param y
+	 * @param width
+	 * @param height
+	 * @param direction
+	 * @param spriteCount
+	 * @return
+	 */
+	public BufferedImage[] getMultipleSprites(int x, int y, int width, int height, Direction direction,
+			int spriteCount) {
+		BufferedImage[] frames = new BufferedImage[spriteCount];
+
+		int frameCount = 0;
+
+		while (frameCount < spriteCount) {
+
+			switch (direction) {
+			case UP:
+				frames[frameCount] = getSectionAt(x, y, width, height);
+				frameCount++;
+				y -= height;
+				break;
+			case DOWN:
+				frames[frameCount] = getSectionAt(x, y, width, height);
+				frameCount++;
+				y += height;
+				break;
+			case LEFT:
+				frames[frameCount] = getSectionAt(x, y, width, height);
+				frameCount++;
+				x -= width;
+				break;
+			case RIGHT:
+				frames[frameCount] = getSectionAt(x, y, width, height);
+				frameCount++;
+				x += width;
+				break;
+			}
+
+		}
+
+		return frames;
+
 	}
 
 	/**
