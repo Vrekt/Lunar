b7c64f8bcfad754d374f3462cd87cef39691701a
Rick Boss <rickeboss@gmail.com>
2017-03-17 17:04:43 -0700

    Add documentation and more helpers to Utilities
    - manhattan distance
    - clamp

--- src/me/vrekt/lunar/utilities/Utilities.java
+++ src/me/vrekt/lunar/utilities/Utilities.java
@@ -8,9 +8,26 @@ public class Utilities {
 		return Math.round(value / nearestValue) * nearestValue;
 	}
 
-	public static double distance(int x, int y, int xx, int yy) {
+    /**
+     * Calculate the Pythagorean distance from (x, y) to (xx, yy)
+     */
+	public static double distance(double x, double y, double xx, double yy) {
         double dx = xx - x;
         double dy = yy - y;
 	    return Math.sqrt(dx*dx + dy*dy);
+    }
+
+    /**
+     * Calculate the Manhattan distance from (x, y) to (xx, yy). Useful for tile based movement.
+     */
+    public static double manhattanDistance(double x, double y, double xx, double yy) {
+        return Math.abs((x - xx) + (y - yy));
+    }
+
+    /**
+     * Clamp a value between a lower and an upper bound
+     */
+    public static double clamp(double value, double lower, double upper) {
+        return Math.max(Math.min(value, upper), lower);
     }
 }
