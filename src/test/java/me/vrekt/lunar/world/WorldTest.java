2a93045e8058341774ce7323b6d8073ae66b2678
Rick Boss <rickeboss@gmail.com>
2017-10-22 15:55:00 -0700

    Finish world entity tests

--- src/test/java/me/vrekt/lunar/world/WorldTest.java
+++ src/test/java/me/vrekt/lunar/world/WorldTest.java
@@ -7,6 +7,9 @@ import me.vrekt.lunar.entity.Entity;
 import me.vrekt.lunar.entity.TestEntity;
 import org.junit.jupiter.api.*;
 
+import java.util.List;
+import java.util.stream.Stream;
+
 /**
  * Created by Rick on 10/21/17.
  */
@@ -25,93 +28,82 @@ class WorldTest {
 
     /** World Entity Tests **/
     @Nested
+    @Tag("world_entity")
     @DisplayName("entity handling")
     class EntityHandling {
         @Nested
-        @DisplayName("adding")
-        class Adding {
+        @DisplayName("manipulate world entity list")
+        class ManipulateWorldEntityList {
             TestEntity ent;
             @BeforeEach
             void addEntity() {
                 ent = new TestEntity();
                 world.addEntity(ent);
             }
-        }
-        @Test
-        @Tag("world_entity")
-        @DisplayName("add entity")
-        void TestAddEntity() {
-            world.addEntity(new TestEntity());
-            assertEquals(1, world.getWorldEntities().size(), "Expected one entity to be added to world entity list.");
+
+            @Test
+            @DisplayName("add entity")
+            void TestAddEntity() {
+                assertEquals(1, world.getWorldEntities().size(), "Expected one entity to be added to world entity list.");
+            }
+
+            @Test
+            @DisplayName("remove entity")
+            void TestRemoveEntity() {
+                world.removeEntity(ent);
+                assertEquals(0, world.getWorldEntities().size(), "Expected entity to be removed.");
+            }
+
+            @Test
+            @DisplayName("remove nonexistent entity")
+            void TestRemoveNonexistentEntity() {
+                TestEntity nonent = new TestEntity();
+                // don't add the entity, but try to remove it
+                world.removeEntity(nonent);
+                assertEquals(1, world.getWorldEntities().size(), "Expected there to still be one entity.");
+            }
+
+            @Test
+            @DisplayName("get entity by id")
+            void TestGetEntity() {
+                Entity got = world.getEntity(ent.getEntityID());
+                assertTrue(got instanceof TestEntity);
+                assertEquals(ent, got, "Expected entity returned to be the one we added.");
+            }
+
+            @Test
+            @DisplayName("remove queued entity on tick")
+            void TestRemoveQueuedEntities() {
+                world.queueEntityForRemoval(ent);
+                world.onTick();
+                assertEquals(0, world.getWorldEntities().size(), "Expected queued entity to be removed after tick.");
+            }
+
+
+            @Test
+            @DisplayName("add queued entity on tick")
+            void TestQueueEntityForAdd() {
+                // remove the one that's automatically added
+                world.removeEntity(ent);
+                world.queueEntityForAdd(ent);
+                world.onTick();
+                assertEquals(1, world.getWorldEntities().size(), "Expected one entity to be added from queue.");
+                assertEquals(ent, world.getWorldEntities().get(0), "Expected the added entity to be our entity.");
+            }
         }
 
         @Test
-        @Tag("world_entity")
-        @DisplayName("remove entity")
-        void TestRemoveEntity() {
-            TestEntity ent = new TestEntity();
-            world.addEntity(ent);
-            world.removeEntity(ent);
-            assertEquals(0, world.getWorldEntities().size(), "Expected entity to be removed.");
-        }
-
-        @Test
-        @Tag("world_entity")
-        @DisplayName("remove nonexistent entity")
-        void TestRemoveNonexistentEntity() {
-            world.addEntity(new TestEntity());
-            TestEntity ent = new TestEntity();
-            // don't add the entity, but try to remove it
-            world.removeEntity(ent);
-            assertEquals(1, world.getWorldEntities().size(), "Expected there to still be one entity.");
-        }
-
-        @Test
-        @Tag("world_entity")
-        @DisplayName("add queued entity on tick")
-        void TestQueueEntityForAdd() {
-            TestEntity ent = new TestEntity();
-            world.queueEntityForAdd(ent);
-            world.onTick();
-            assertEquals(1, world.getWorldEntities().size(), "Expected one entity to be added from queue.");
-            assertEquals(ent, world.getWorldEntities().get(0), "Expected the added entity to be our entity.");
-        }
-
-        @Test
-        @Tag("world_entity")
-        @DisplayName("remove queued entity on tick")
-        void TestRemoveQueuedEntities() {
-            TestEntity ent = new TestEntity();
-            world.addEntity(ent);
-            world.queueEntityForRemoval(ent);
-            world.onTick();
-            assertEquals(0, world.getWorldEntities().size(), "Expected queued entity to be removed after tick.");
-        }
-
-        @Test
-        @Tag("world_entity")
-        @DisplayName("get entity by id")
-        void TestGetEntity() {
-            TestEntity ent = new TestEntity();
-            world.addEntity(ent);
-            Entity got = world.getEntity(ent.getEntityID());
-            assertTrue(got instanceof TestEntity);
-            assertEquals(ent, got, "Expected entity returned to be the one we added.");
-        }
-
-        @Test
-        @Tag("world_entity")
         @DisplayName("is entity at a location")
         void TestIsEntityAt() {
             TestEntity ent = new TestEntity();
             ent.setX(15);
             ent.setY(15);
             world.addEntity(ent);
-            assertTrue(world.isEntityAt(15, 15), "Expected to find our entity at the given coordinats.");
+            assertTrue(world.isEntityAt(15, 15), "Expected to find our entity at the given coordinates.");
         }
 
+
         @Test
-        @Tag("world_entity")
         @DisplayName("get entity at a location")
         void TestGetEntityAt() {
             TestEntity ent = new TestEntity();
@@ -119,14 +111,12 @@ class WorldTest {
             ent.setY(15);
             world.addEntity(ent);
 
-            assertEquals(ent, world.getEntityAt(15, 15));
+            assertEquals(ent, world.getEntityAt(15, 15), "Expected to get back our entity from coordinates.");
         }
 
         @Test
-        @Tag("world_entity")
         @DisplayName("get list of world entities")
         void TestGetWorldEntities() {
-            world.addEntity(new TestEntity());
             assertEquals(world.worldEntities, world.getWorldEntities(), "Expected to get the correct list of entities");
         }
     }
